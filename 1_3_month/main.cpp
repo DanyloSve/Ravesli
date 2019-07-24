cfda25017c7fa23bc5962384bd50d8fe0c51cbc5
DanyloSve <48258707+DanyloSve@users.noreply.github.com>
2019-07-24 22:46:22 +0300

    Update main.cpp

--- 1_3_month/main.cpp
+++ 1_3_month/main.cpp
@@ -2,65 +2,34 @@
 
 int main(int argc, char *argv[])
 {
-    int lmonth;
-    std::cin >> lmonth;
+    int lMonth;
+    std::cin >> lMonth;
 
+            if (lMonth == 1 || lMonth == 2 || lMonth == 12)
+            {
+               std::cout << "Winter";
+            }
 
+            else if (lMonth >= 3 && lMonth <= 5)
+            {
+               std::cout << "Spring";
+            }
 
-        switch(lmonth)
-        {
+            else if (lMonth >= 6 && lMonth <= 8)
+            {
+                std::cout << "Summer";
+            }
 
-        case 1:
-            std::cout << "January";
-            break;
+            else if (lMonth >= 9 && lMonth <= 11)
+            {
+               std::cout << "Autumn";
+            }
 
-        case 2:
-            std::cout << "February";
-            break;
+            else
+            {
+                std::cout << "Your input is not correct !";
+            }
 
-        case 3:
-            std::cout << "March";
-            break;
-
-        case 4:
-            std::cout << "April";
-            break;
-
-        case 5:
-            std::cout << "May";
-            break;
-
-        case 6:
-            std::cout << "June";
-            break;
-
-        case 7:
-            std::cout << "July";
-            break;
-
-        case 8:
-            std::cout << "August";
-            break;
-
-        case 9:
-            std::cout << "September";
-            break;
-
-        case 10:
-            std::cout << "October";
-            break;
-
-        case 11:
-            std::cout << "November";
-            break;
-
-        case 12:
-            std::cout << "December";
-            break;
-
-        default:
-            std::cout << "Your input is not correct !";
-            break;
-   }
+            std::cout<< '\n';
     return 0;
 }
