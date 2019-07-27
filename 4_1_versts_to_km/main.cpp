351624eaf6268b6d915ab5345ef5b697ed327443
21828 <nightcondor@ukr.net>
2019-07-27 15:37:51 +0300

    fully working programme

---
+++ 4_1_versts_to_km/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+   int versts (0);
+   std::cout << "Input versts:\n";
+   std::cin  >> versts;
+   std::cout << versts << ' ' << "versts = "<< 1.0668 * versts <<"km\n";
+
+    return 0;
+}
