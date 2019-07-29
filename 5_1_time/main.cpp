5eadf74d52e08d50e6327010683394d50ba2bf42
21828 <nightcondor@ukr.net>
2019-07-29 16:54:18 +0300

    Fully working programme

---
+++ 5_1_time/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Input time in minutes:\n";
+    int minutes;
+    std::cin >> minutes;
+    std::cout << "Hours :"<< ' ' << minutes / 60 << '\n';
+    std::cout << "Minutes :"<< ' ' << minutes % 60 << '\n';
+    return 0;
+}
