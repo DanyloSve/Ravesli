fd97357283d675c3fd354bb670820c43bfea7789
21828 <nightcondor@ukr.net>
2019-07-29 17:08:42 +0300

    Fully working programme!

---
+++ 5_2_price_for_the_apples/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Input price for 1kg of apples:\n";
+    double price;
+    std::cin >> price;
+
+    std::cout << "wage"  << '\t' << "price\n";
+
+    for(int i(100); i < 1000; i+=100)
+    {
+        std::cout << i << ' ' << "g" << " = " << price / 1000 * i <<'\n';
+    }
+    return 0;
+}
