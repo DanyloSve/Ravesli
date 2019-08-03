beff61275fb66a758676cfc02239e8397875073b
21828 <nightcondor@ukr.net>
2019-08-03 20:48:47 +0300

    Fully working programme!

--- 9_2_array/main.cpp
+++ 9_2_array/main.cpp
@@ -2,6 +2,8 @@
 
 namespace myNamespace
 {
+    //створення масиву
+
     char **createArr(int a, int b)
     {
         char **m = new char *[a];
@@ -12,43 +14,47 @@ namespace myNamespace
                 m[j] = m [j - 1] + b;
             }
 
-
-
-        for (int i(0); i != a; i++)
+        //заповнення масиву елементами, що вивести неініціалізований масив
+        for (int i(0); i != a ; i++)
         {
             for (int j(1); j != b + 1; j++)
             {
-                m[i][j] = '_';
+                m[i][j] = ':';
             }
         }
         return m;
     }
 
+    //видалення масиву
     void deleteArr(char **m, int a)
     {
         for (int i(0); i != a; i++)
         {
-            delete [] m[i];
+            delete [] m[0];
+            delete [] m;
         }
 
         delete [] m;
     }
 
+    //виводення масиву після кожного введення елементу у консоль
     void vizualize (char **m, int a, int b)
     {
         for (int i(-1); i != a + 1; i++)
         {
-            if (i == -1 || i == a)
-            {
-             std::cout << '#';
-            }
+
             for (int j(0); j != b + 2; j++)
             {
-                if (j == 0 || j == b + 1)
+                if (i == -1 || i == a)
+                {
+                 std::cout << '#';
+
+                }
+                else if (j == 0 || j == b + 1)
                 {
                     std::cout << '#';
                 }
-                else
+                else if ( (i != -1 || i != a) || (j != 0 || j != b + 1))
                 {
                     std::cout << m[i][j];
                 }
@@ -57,22 +63,60 @@ namespace myNamespace
         }
     }
 
+    //ініціалізування масиву
+
     void initialize(char **m, int a, int b)
     {
         vizualize(m, a, b);
         char iteam;
+
         for (int i(0); i != a; i++)
         {
-            for (int j(1); j != b; j++)
+            for (int j(1); j != b + 1; j++)
             {
                 std::cin >> iteam;
                 m[i][j] = iteam;
+
                 vizualize(m, a, b);
             }
         }
+
+       }
+
+    void search(char** m, int a, int b, char search)
+    {
+        bool found = false;
+        for(int i(0); i != a; i++)
+        {
+            for (int j(1); j != b + 1; j++)
+            {
+                if (m[i][j] != search)
+                {
+                    m[i][j] = '.';
+                }
+                else
+                {
+                        found = true;
+
+                }
+            }
+        }
+
+        vizualize(m, a,b);
+        if(found)
+        {
+            std::cout << "Your character is found!\n";
+
+        }
+        else
+        {
+            std::cout << "Sorry...\n"
+                      << "I did not found your character(\n";
+
+        }
     }
 
-
+    //інтерфейс програми(незавершений)
     void userInterface()
     {
         int i(0);
@@ -97,12 +141,23 @@ namespace myNamespace
             std::cout << "Input number of columns:\n";
         }
 
-        //initialize(createArr(i, j), i, j);
+        std::cout << "Input iteams of array:\n";
 
-        vizualize ((createArr(i, j)), i, j);
-        deleteArr(createArr(i, j),i);
+        char** m = createArr(i,j);
+        initialize(m, i, j);
 
-    }
+        std::cout << "Input iteam to search:\n";
+        char toFind;
+
+        while (!(std::cin >> toFind))
+        {
+            std::cin.clear();
+            std::cin.ignore(3000, '\n');
+         }
+        search(m, i,j,toFind);
+
+        deleteArr(m,i);
+     }
 }
 
 int main()
