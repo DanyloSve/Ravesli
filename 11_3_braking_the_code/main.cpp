db1da781e3e66e6b6d1b1f7d189d7f7996b6984c
21828 <nightcondor@ukr.net>
2019-08-07 14:59:30 +0300

    Problem: does not work properly!

---
+++ 11_3_braking_the_code/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Enter the code:\n";
+    int code(0);
+    std::cin >> code;
+
+    int ones(code % 10);
+    int hundrets(code / 100);
+    int dec = (code - ones - hundrets * 100) / 10;
+
+    int count(0);
+
+    for (int i(0); i < 10; i++)
+    {
+        for (int j(0); j < 10; j++)
+        {
+            for (int k(0); k < 10; k++)
+            {
+                if ( k == ones
+                     && j == dec
+                     && i ==  hundrets)
+                {
+                    break;
+                }
+
+                count ++;
+            }
+        }
+    }
+
+    std::cout << "It takes " << count << " conmbinations to break the code\n";
+
+    return 0;
+}
