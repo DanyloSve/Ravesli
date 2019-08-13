8806790728f5927b5648f20f784fd793bf7cb2e7
21828 <nightcondor@ukr.net>
2019-08-13 13:29:15 +0300

    Problem: Console output works incorrect!

--- 12_3_communications/main.cpp
+++ 12_3_communications/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <vector>
 #include <random>
+#include <unistd.h>
 
 class Person
 {
 protected:
     std::string mName;
     std::string mPersonType;
-    int mYear;
-
- };
+    int mAge;
+};
 
 class Formalists : public Person
 {
@@ -19,9 +20,24 @@ public:
     Formalists(std::string name, int year)
     {
        mName = name;
-       mYear = year;
+       mAge = year;
        std::string mGreeting = "Hello!";
        mPersonType = "formal";
+    }
+
+    std::string getGreeting()
+    {
+        return mGreeting;
+    }
+
+    int getAge()
+    {
+        return mAge;
+    }
+
+    std::string getName()
+    {
+        return mName;
     }
 
 };
@@ -35,12 +51,26 @@ public:
     Neoformalists(std::string name, int year)
     {
         mName = name;
-        mYear = year;
+        mAge = year;
 
         std::string mGreeting = "Hi";
         mPersonType = "neoformal";
     }
 
+    std::string getGreeting()
+    {
+        return mGreeting;
+    }
+
+    int getAge()
+    {
+        return mAge;
+    }
+
+    std::string getName()
+    {
+        return mName;
+    }
 };
 
 class Realists : Person
@@ -52,26 +82,37 @@ public:
     Realists(std::string name, int year)
     {
         mName = name;
-        mYear = year;
+        mAge = year;
         mPersonType = "realist";
+        mGreeting = "Hello";
     }
 
     std::string getGreeting(int yearDifference)
     {
         if (yearDifference <= 5)
         {
-            return mGreeting = "Hi";
+            mGreeting = "Hi";
+            return mGreeting;
         }
         else
         {
-            return mGreeting = "Hello";
+            mGreeting = "Hello";
+            return mGreeting;
         }
     }
 
+    int getAge()
+    {
+        return mAge;
+    }
 
+    std::string getName()
+    {
+        return mName;
+    }
 };
 
-int year()
+int age()
 {
     std::random_device rd;
     std::mt19937 marsenne(rd());
@@ -84,7 +125,7 @@ int year()
     }
     else
     {
-        year();
+       age();
     }
 }
 
@@ -98,29 +139,135 @@ int type()
 
     if (rand >= 1 && rand <= 3)
     {
-          return rand;
+          if (rand == 1)
+          {
+             return rand;
+          }
     }
     else
     {
         type();
     }
 }
+
 void usrInfce()
 {
+    Formalists Olena("Olena", age());
 
-    Olexander().setName("Olexander");
-    Olexander().setYear(year());
-    Olexander().
+    Neoformalists Andriy("Andriy", age());
+    Neoformalists Igor("Igor", age());
 
-    Person Igor();
-    Person Anna();
-    Person Danylo();
-    Person Andriy();
+    Realists Tanya("Tanya", age());
+    Realists Danylo("Danylo", age());
 
+
+    //Olena - Danylo
+    std::cout << Olena.getGreeting();
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Danylo.getName();
+     sleep(1);
+    std::cout<< " ! " << " \n ";
+     sleep(2);
+
+    //Danylo - Olena
+    std::cout << Danylo.getGreeting(Olena.getAge() - Danylo.getAge());
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Olena.getName();
+     sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2);
+
+
+    //Danylo - Andriy
+    std::cout << Danylo.getGreeting(Igor.getAge() - Danylo.getAge());
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Igor.getName();
+     sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2)
+             ;
+    //Andriy - Danylo
+    std::cout << Igor.getGreeting();
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Danylo.getName();
+     sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2);
+
+
+    //Andriy - Igor
+    std::cout << Andriy.getGreeting();
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout<< Igor.getName();
+     sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2);
+
+    //Igor - Andriy
+    std::cout << Igor.getGreeting();
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Andriy.getName();
+     sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2);
+
+
+    //Igor - Tanya
+    std::cout << Igor.getGreeting();
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Tanya.getName();
+     sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2);
+
+    //Tanya - Igor
+    std::cout << Tanya.getGreeting(Igor.getAge() - Tanya.getAge());
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Igor.getName();
+     sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2);
+
+
+    //Tanya - Danylo
+    std::cout << Tanya.getGreeting(Danylo.getAge() - Tanya.getAge());
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Danylo.getName();
+     sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2);
+
+    //Danylo - Tanya
+    std::cout << Danylo.getGreeting(Tanya.getAge() - Danylo.getAge());
+     sleep(1);
+    std::cout << " , ";
+     sleep(1);
+    std::cout << Tanya.getName();
+    sleep(1);
+    std::cout << " ! " << " \n ";
+     sleep(2);
 }
+
 int main(int argc, char *argv[])
 {
-
-
+    usrInfce();
     return 0;
 }
