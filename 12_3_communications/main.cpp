b90a30a12107daf14d1213dea8e46644d2686eb8
21828 <nightcondor@ukr.net>
2019-08-13 14:06:46 +0300

    Problem: Age is not correctly inizilizing, greeting is not correctly outputing!

--- 12_3_communications/main.cpp
+++ 12_3_communications/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <random>
 #include <unistd.h>
 
@@ -9,6 +9,23 @@ protected:
     std::string mName;
     std::string mPersonType;
     int mAge;
+
+public:
+    std::string getPersonType()
+    {
+        return mPersonType;
+    }
+
+    int getAge()
+    {
+        return mAge;
+    }
+
+    std::string getName()
+    {
+        return mName;
+    }
+
 };
 
 class Formalists : public Person
@@ -30,19 +47,9 @@ public:
         return mGreeting;
     }
 
-    int getAge()
-    {
-        return mAge;
-    }
-
-    std::string getName()
-    {
-        return mName;
-    }
-
 };
 
-class Neoformalists : Person
+class Neoformalists : public Person
 {
 private:
     std::string mGreeting;
@@ -62,18 +69,9 @@ public:
         return mGreeting;
     }
 
-    int getAge()
-    {
-        return mAge;
-    }
-
-    std::string getName()
-    {
-        return mName;
-    }
 };
 
-class Realists : Person
+class Realists : public Person
 {
 private:
     std::string mGreeting;
@@ -99,16 +97,6 @@ public:
             mGreeting = "Hello";
             return mGreeting;
         }
-    }
-
-    int getAge()
-    {
-        return mAge;
-    }
-
-    std::string getName()
-    {
-        return mName;
     }
 };
 
@@ -161,109 +149,139 @@ void usrInfce()
     Realists Danylo("Danylo", age());
 
 
+    //Olena intro
+    std::cout << "My name is " << Olena.getName() << " . \n";
+    std::cout << "I am " << Olena.getAge() << " years old. \n";
+    std::cout << "And I am " << Olena.getPersonType() << " . \n";
+    std::cout << "\n";
+
+    //Andriy intro
+    std::cout << "My name is " << Andriy.getName() << " . \n";
+    std::cout << "I am " << Andriy.getAge() << " years old. \n";
+    std::cout << "And I am " << Andriy.getPersonType() << " . \n";
+    std::cout << "\n";
+
+    //Igor intro
+    std::cout << "My name is " << Igor.getName() << " . \n";
+    std::cout << "I am " << Igor.getAge() << " years old. \n";
+    std::cout << "And I am " << Igor.getPersonType() << " . \n";
+    std::cout << "\n";
+
+    //Tanya intro
+    std::cout << "My name is " << Tanya.getName() << " . \n";
+    std::cout << "I am " << Tanya.getAge() << " years old. \n";
+    std::cout << "And I am " << Tanya.getPersonType() << " . \n";
+    std::cout << "\n";
+
+    //Danylo intro
+    std::cout << "My name is " << Danylo.getName() << " . \n";
+    std::cout << "I am " << Danylo.getAge() << " years old. \n";
+    std::cout << "And I am " << Danylo.getPersonType() << " . \n";
+    std::cout << "\n";
+
     //Olena - Danylo
     std::cout << Olena.getGreeting();
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Danylo.getName();
-     sleep(1);
+     //sleep(1);
     std::cout<< " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 
     //Danylo - Olena
     std::cout << Danylo.getGreeting(Olena.getAge() - Danylo.getAge());
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Olena.getName();
-     sleep(1);
+     //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 
 
     //Danylo - Andriy
     std::cout << Danylo.getGreeting(Igor.getAge() - Danylo.getAge());
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Igor.getName();
-     sleep(1);
+     //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2)
-             ;
+     //sleep(2);
+
     //Andriy - Danylo
     std::cout << Igor.getGreeting();
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Danylo.getName();
-     sleep(1);
+     //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 
 
     //Andriy - Igor
     std::cout << Andriy.getGreeting();
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout<< Igor.getName();
-     sleep(1);
+     //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 
     //Igor - Andriy
     std::cout << Igor.getGreeting();
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Andriy.getName();
-     sleep(1);
+     //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 
 
     //Igor - Tanya
     std::cout << Igor.getGreeting();
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Tanya.getName();
-     sleep(1);
+     //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 
     //Tanya - Igor
     std::cout << Tanya.getGreeting(Igor.getAge() - Tanya.getAge());
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Igor.getName();
-     sleep(1);
+     //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 
 
     //Tanya - Danylo
     std::cout << Tanya.getGreeting(Danylo.getAge() - Tanya.getAge());
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Danylo.getName();
-     sleep(1);
+     //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 
     //Danylo - Tanya
     std::cout << Danylo.getGreeting(Tanya.getAge() - Danylo.getAge());
-     sleep(1);
+     //sleep(1);
     std::cout << " , ";
-     sleep(1);
+     //sleep(1);
     std::cout << Tanya.getName();
-    sleep(1);
+    //sleep(1);
     std::cout << " ! " << " \n ";
-     sleep(2);
+     //sleep(2);
 }
 
 int main(int argc, char *argv[])
