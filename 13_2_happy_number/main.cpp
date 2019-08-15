eaca26dfbd1b658992d6202c603d1801649e0d88
21828 <nightcondor@ukr.net>
2019-08-15 11:53:36 +0300

    Fully working programme!

---
+++ 13_2_happy_number/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int numb(0);
+    std::cin >> numb;
+
+    int result = 0;
+    int n[6];
+
+    n[0] = numb / 100000;
+    n[5] = numb % 10;
+
+    n[4] = (numb % 100 - n[5]) / 10 ;
+    n[3] = (numb % 1000 - numb % 100) / 100;
+    n[2] = (numb % 10000 - numb % 1000) / 1000;
+    n[1] = (numb % 100000 - numb % 10000) / 10000;
+
+    for (int i(0), j(5); i != 5; i++, j--)
+    {
+        result += n[i] - n[j];
+    }
+
+    (result == 0) ? (std::cout << true) : (std::cout << false);
+
+    std::cout << '\n';
+    return 0;
+}
