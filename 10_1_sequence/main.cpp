76471a6d83946d355642b3b915d2c012f2fdfbbf
21828 <nightcondor@ukr.net>
2019-08-25 18:02:29 +0300

    Fully working programme!

--- 10_1_sequence/main.cpp
+++ 10_1_sequence/main.cpp
@@ -1,65 +1,22 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-
+#include <vector>
 
 namespace mySpace
 {
 
-    double array()
-    {
-        std::string input;
-        std::getline(std::cin, input);
-
-        int buffer(2);
-        int position(0);
-
-        double *number = new double[buffer];
-
-        std::string temp;
-
-        for (int i(0), j(0); input[i] != '\n'; i++)
-        {
-            if (input[i] == ' ')
-            {
-                std::stringstream convertion(input);
-
-                if (buffer == position)
-                {
-                    double *tmp = new double [buffer * 2];
-
-                    for (int k(0); k < buffer / 2; k++)
-                    {
-                        tmp[k] = number[k];
-                    }
-                    delete [] number;
-                    number = tmp;
-                }
-
-                convertion >> number[position];
-                position++;
-                j = 0;
-            }
-            else
-            {
-                temp[j] = input[i]; // тут не ініціалізується temp
-                j++;
-            }
-        }
-
-        double sum(0);
-        for (int i(0); i != position; i++)
-        {
-            sum += number[i];
-        }
-        return sum / position;
-    }
-
-
-
     void usrInfce()
     {
-        std::cout << array();
+
+        double a(0);
+        double sum(0);
+        int count(0);
+        while (std::cin >> a)
+        {
+            sum += a;
+            count++;
+        }
+        std::cout << "Average : " << sum / count << '\n';
     }
 }
 
